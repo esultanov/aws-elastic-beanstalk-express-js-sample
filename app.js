9df2f75192eaba7b7093a0a991a961a1b7217535
Elnur S <esultanov6@gmail.com>
2021-05-19 06:53:24 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('NEdiyee'));
+app.get('/', (req, res) => res.send('Xoshuma gelira'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
